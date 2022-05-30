2090f9b5251b07b5f849ab9fde3ec6179e7a47fd
han254 <peternjenga1706@gmail.com>
2022-05-30 21:59:03 +0300

    initial commit

---
+++ app/src/main/java/com/moringaschool/adoptpet/petSearchActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.adoptpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class petSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_search);
+    }
+}
