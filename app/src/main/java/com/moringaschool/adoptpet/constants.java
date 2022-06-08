f8bce1521e813bc9fcce99b62fb77c8de66931df
han254 <peternjenga1706@gmail.com>
2022-06-08 22:28:13 +0300

    add constants class

---
+++ app/src/main/java/com/moringaschool/adoptpet/constants.java
@@ -0,0 +1,7 @@
+package com.moringaschool.adoptpet;
+
+public class constants {
+    public static final String Petfinder_BASE_URL = "https://www.petfinder.com/developers/v2";
+    public static final String Petfinder_API_KEY = BuildConfig.Petfinder_API_KEY;
+
+}
