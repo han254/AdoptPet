71f6f878084a6662943a27f1173e5ec21ee0e999
han254 <peternjenga1706@gmail.com>
2022-06-13 12:41:21 +0300

    implement a pagerAdapter

--- app/src/main/java/adapters/petListAdapter.java
+++ app/src/main/java/adapters/petListAdapter.java
@@ -1,6 +1,8 @@
 package adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,58 +13,72 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.adoptpet.R;
 import com.moringaschool.adoptpet.models.Animal;
+import com.moringaschool.adoptpet.ui.petDetailActivity;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class petListAdapter extends RecyclerView.Adapter<petListAdapter.petViewHolder> {
+public class petListAdapter extends RecyclerView.Adapter<petListAdapter.PetViewHolder> {
     private List<Animal> mPets;
     private Context mContext;
 
-    public petListAdapter(Context context, List<Animal> pets) {
-        mContext = context;
-        mPets =pets;
-    }
-    public class petViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.petImageView) ImageView petImageView;
-        @BindView(R.id.petNameTextView) TextView mNameTextView;
-        @BindView(R.id.breedTextView) TextView mBreedTextView;
-        @BindView(R.id.contactTextView) TextView mContactTextView;
 
-        private Context mContext;
-
-        public petViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-            mContext = itemView.getContext();
-
+        public petListAdapter(Context context, List<Animal> pets) {
+            mContext = context;
+            mPets = pets;
         }
 
-        public void bindPet(Animal pet) {
-//            Picasso.get().load(pet.getPhotos().into(petImageView));
-            mNameTextView.setText(pet.getName());
-            mBreedTextView.setText(pet.getBreeds().getPrimary());
-            mContactTextView.setText(pet.getContact().getEmail());
+        @NonNull
+        @Override
+        public petListAdapter.PetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pet_list_item, parent, false);
+            PetViewHolder viewHolder = new PetViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(petListAdapter.PetViewHolder holder, int position) {
+            holder.bindPet(mPets.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPets.size();
+        }
+
+        public class PetViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+            @BindView(R.id.petImageView) ImageView mPetImageView;
+            @BindView(R.id.petNameTextView) TextView mPetNameTextView;
+            @BindView(R.id.breedTextView) TextView mBreedTextView;
+            @BindView(R.id.contactTextView) TextView mContactTextView;
+
+            private Context mContext;
+
+            public PetViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+                mContext = itemView.getContext();
+                itemView.setOnClickListener(this);
+            }
+
+            public void bindPet(Animal pet) {
+                mPetNameTextView.setText(pet.getName());
+                mBreedTextView.setText(pet.getBreeds().getPrimary());
+                mContactTextView.setText(pet.getContact().getEmail());
+            }
+
+            @Override
+            public void onClick(View v) {
+                int itemPosition = getLayoutPosition();
+                Intent intent = new Intent(mContext, petDetailActivity.class);
+                intent.putExtra("position", itemPosition);
+                intent.putExtra("pets", Parcels.wrap(mPets));
+                mContext.startActivity(intent);
+            }
         }
     }
-    @Override
-    public petListAdapter.petViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pet_list_item, parent, false);
-        petViewHolder viewHolder = new petViewHolder(view);
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(petListAdapter.petViewHolder holder, int position) {
-        holder.bindPet(mPets.get(position));
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return mPets.size();
-    }
-}
--- app/src/main/java/com/moringaschool/adoptpet/ui/petDetailActivity.java
+++ app/src/main/java/com/moringaschool/adoptpet/ui/petDetailActivity.java
@@ -1,16 +1,26 @@
 package com.moringaschool.adoptpet.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.moringaschool.adoptpet.R;
+import com.moringaschool.adoptpet.models.Animal;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import adapters.petPagerAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class petDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private petPagerAdapter adapterViewPager;
+    List<Animal> mPets;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pet_detail);
-    }
+
 }
