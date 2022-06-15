eb11cb0f79694fd661292df1b32ae43f82532ace
han254 <peternjenga1706@gmail.com>
2022-06-16 00:17:32 +0300

    writing POJO :Firebase

--- app/src/main/java/com/moringaschool/adoptpet/constants.java
+++ app/src/main/java/com/moringaschool/adoptpet/constants.java
@@ -5,5 +5,7 @@ public class constants {
     public static final String Petfinder_BASE_URL = "https://api.petfinder.com/v2/";
     public static final String Petfinder_API_KEY = BuildConfig.Petfinder_API_KEY;
     public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedlocation";
+    public static final String FIREBASE_CHILD_PETS = "pets";
+
 
 }
--- app/src/main/java/com/moringaschool/adoptpet/ui/petDetailFragment.java
+++ app/src/main/java/com/moringaschool/adoptpet/ui/petDetailFragment.java
@@ -10,8 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.adoptpet.R;
+import com.moringaschool.adoptpet.constants;
 import com.moringaschool.adoptpet.models.Animal;
 
 
@@ -22,7 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class petDetailFragment extends Fragment {
+public class petDetailFragment extends Fragment implements View.OnClickListener{
     @BindView(R.id.petImageView) ImageView petImageView;
     @BindView(R.id.petNameTextView) TextView mNameTextView;
     @BindView(R.id.contactTextView) TextView mContactTextView;
@@ -63,6 +67,20 @@ public class petDetailFragment extends Fragment {
         mContactTextView.setText(mPet.getContact().getEmail());
         mPhoneLabel.setText(mPet.getContact().getEmail());
 
+        mSavePetButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == mSavePetButton) {
+            DatabaseReference restaurantRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(constants.FIREBASE_CHILD_PETS);
+            restaurantRef.push().setValue(mPet);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
