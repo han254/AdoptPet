a45cce87a3dfea1485a5c0273951c93ccf437787
han254 <peternjenga1706@gmail.com>
2022-06-16 03:41:06 +0300

    validate the login to move to the signup

--- app/src/main/java/com/moringaschool/adoptpet/ui/Login.java
+++ app/src/main/java/com/moringaschool/adoptpet/ui/Login.java
@@ -2,15 +2,34 @@ package com.moringaschool.adoptpet.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.adoptpet.R;
 
-public class Login extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRegisterTextView) {
+            Intent intent = new Intent(Login.this, SignUp.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
